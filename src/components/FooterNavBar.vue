5332d886cc6213d9ffe5f06e9d1dc85c750512b9
Eric.Lee <liwei7016@users.noreply.github.com>
2019-02-15 11:03:44 +0800

    修改footernavbar，增加图标

--- src/components/FooterNavBar.vue
+++ src/components/FooterNavBar.vue
@@ -1,29 +1,97 @@
 <template>
   <div class="footerbar-nav">
-    <mt-tabbar v-model="selected">
-    <mt-tab-item id="tab1">
-      <a href="/meet" class="footerbar-link">邂逅</a>
-    </mt-tab-item>
-    <mt-tab-item id="tab2">
-      <a href="/search" class="footerbar-link">搜索</a>
-    </mt-tab-item>
-    <mt-tab-item id="tab3">
-      <a href="/message" class="footerbar-link">消息</a>
-    </mt-tab-item>
-    <mt-tab-item id="tab4">
-      <a href="/vip" class="footerbar-link">VIP</a>
-    </mt-tab-item>
-    <mt-tab-item id="tab5">
-      <a href="/myinfo" class="footerbar-link">我的</a>
-    </mt-tab-item>
-  </mt-tabbar>
+    <mt-tabbar>
+      <mt-tab-item id="tab1" class="footerbar-tab">
+        <router-link tag="a" to="/meet" class="footerbar-link meet" activeClass="active" exact>
+          <p class="title">邂逅</p>
+        </router-link>
+      </mt-tab-item>
+      <mt-tab-item id="tab2" class="footerbar-tab">
+        <router-link tag="a" to="/search" class="footerbar-link search" activeClass="active" exact>
+          <p class="title">搜索</p>
+        </router-link>
+      </mt-tab-item>
+      <mt-tab-item id="tab3" class="footerbar-tab">
+        <router-link tag="a" to="/message" class="footerbar-link message" activeClass="active" exact>
+          <p class="title">消息</p>
+        </router-link>
+      </mt-tab-item>
+      <mt-tab-item id="tab4" class="footerbar-tab">
+        <router-link tag="a" to="/vip" class="footerbar-link vip" activeClass="active" exact>
+          <p class="title">VIP</p>
+        </router-link>
+      </mt-tab-item>
+      <mt-tab-item id="tab5" class="footerbar-tab">
+        <router-link tag="a" to="/myinfo" class="footerbar-link title" activeClass="active" exact>
+          <p class="title">我的</p>
+        </router-link>
+      </mt-tab-item>
+    </mt-tabbar>
   </div>
 </template>
 
-<style type="text/css">
-  .footerbar-link{
-    display: block;
+<style type="text/css" lang="scss">
+  .footerbar-nav{
+    height:49px!important;
+    .footerbar-tab{
+      .active{
+        .title{
+          color:#ff5e00;
+        }
+      }
+      .footerbar-link{
+        position: relative;
+        display: block;
+        padding-top: 22px;
+        margin: 0 0 0 -14px;
+        .title{
+          height:20px;
+          line-height: 20px;
+          font-size: 12px;
+          text-align: center;
+          padding-left: 26px;
+        }
+      }
+    }
   }
+  .footerbar-link::before{
+    content:"";
+    width: 28px;
+    height: 22px;
+    position: absolute;
+    top:0;
+    left: 50%;
+    background: url("../../public/vender/images/navBottBg.png") no-repeat center top;
+    background-size: 100% auto;
+  }
+  .footerbar-tab:nth-child(1) .footerbar-link.active::before{
+    background-position: 0 -110px;
+  }
+  .footerbar-tab:nth-child(2) .footerbar-link::before{
+    background-position: 0 -22px;
+  }
+  .footerbar-tab:nth-child(2) .footerbar-link.active::before{
+    background-position: 0 -132px;
+  }
+  .footerbar-tab:nth-child(3) .footerbar-link::before{
+    background-position: 0 -44px;
+  }
+  .footerbar-tab:nth-child(3) .footerbar-link.active::before{
+    background-position: 0 -264px;
+  }
+  .footerbar-tab:nth-child(4) .footerbar-link::before{
+    background-position: 0 -219px;
+  }
+  .footerbar-tab:nth-child(4) .footerbar-link.active::before{
+    background-position: 0 -241px;
+  }
+  .footerbar-tab:nth-child(5) .footerbar-link::before{
+    background-position: 0 -88px;
+  }
+  .footerbar-tab:nth-child(5) .footerbar-link.active::before{
+    background-position: 0 -198px;
+  }
+
 </style>
 
 <script type="text/javascript">
@@ -36,10 +104,6 @@
   Vue.component(TabItem.name, TabItem);
 
   export default{
-    data () {
-      return {
-        selected: false
-      }
-    },
+
   }
 </script>
