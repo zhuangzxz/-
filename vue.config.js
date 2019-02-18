53d911b729664f7e547c874066119fea354cba95
Eric.Lee <liwei7016@users.noreply.github.com>
2019-02-19 00:20:59 +0800

    增加反向代理host字段配置

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   devServer: {
-    // port:8000, //随便改端口号
     proxy: {
       '/data': {
         target: 'http://localhost:8061/',
+        host:'http://localhost',
         changeOrigin: true
         // pathRewrite: {
         //     '^/v4/api': '/v4/api'
